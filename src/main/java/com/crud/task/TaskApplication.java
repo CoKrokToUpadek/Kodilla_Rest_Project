8020f3120e7483ead6b72386548dc5ef82834036
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-09-06 19:08:39 +0200

    21.3 crud script

--- src/main/java/com/crud/task/TaskApplication.java
+++ src/main/java/com/crud/task/TaskApplication.java
@@ -1,13 +1,21 @@
 package com.crud.task;
 
+import com.crud.task.domain.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskApplication {
+public class TaskApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+		return application.sources(TaskApplication.class);
 	}
 
 }
--- src/main/java/com/crud/task/controller/TaskController.java
+++ src/main/java/com/crud/task/controller/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
 public class TaskController {
