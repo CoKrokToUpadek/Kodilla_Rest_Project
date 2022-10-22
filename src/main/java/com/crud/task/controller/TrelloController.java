3df5dd37a14284e9a5b63d924571d9eb894a623a
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-10-22 18:59:34 +0200

    24.4

--- src/main/java/com/crud/task/controller/TrelloController.java
+++ src/main/java/com/crud/task/controller/TrelloController.java
@@ -4,6 +4,7 @@ package com.crud.task.controller;
 import com.crud.task.domain.CreatedTrelloCard;
 import com.crud.task.domain.TrelloBoardDto;
 import com.crud.task.domain.TrelloCardDto;
+import com.crud.task.service.TrelloService;
 import com.crud.task.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,58 +19,69 @@ import java.util.Optional;
 @CrossOrigin("*")
 public class TrelloController {
 
-    private final TrelloClient trelloClient;
+    private final TrelloService trelloService;
 
-    @GetMapping("boards")
-    public void getTrelloBoards(@RequestParam(required = false) boolean filter) {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        if(!filter){
-            trelloBoards.forEach(trelloBoardDto -> {
-                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-            });
-        }else {
-            trelloBoards.forEach(trelloBoardDto -> {
-                if (!trelloBoardDto.getName().isEmpty() && trelloBoardDto.getName().contains("Kodilla") && !trelloBoardDto.getId().isEmpty()){
-                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-                }
-            });
-        }
-    }
-
-    @GetMapping("boardsWithResponse")
-    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoardsWithResponse(){
-        return ResponseEntity.ok(trelloClient.getTrelloBoardsWithLogs());
-    }
-
-    @PostMapping("cardsWithResponse")
-    public ResponseEntity<CreatedTrelloCard> createTrelloCardWithResponse(@RequestBody TrelloCardDto trelloCardDto){
-        return ResponseEntity.ok(trelloClient.createNewCard(trelloCardDto));
-    }
-
-
-    @GetMapping("boards2")
-    public List<TrelloBoardDto> getTrelloBoardsV2() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
-        return trelloBoards;
-    }
 
     @PostMapping("cards")
-    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard test=trelloClient.createNewCard(trelloCardDto);
-        //for testing purpose
-        System.out.println(test);
-        return test;
-
+    public ResponseEntity<CreatedTrelloCard> createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+      return ResponseEntity.ok(trelloService.createTrelloCard(trelloCardDto));
     }
+
+    @GetMapping("boards2")
+    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoards(){
+        return ResponseEntity.ok(trelloService.fetchTrelloBoards());
+    }
+
+//    @GetMapping("boards")
+//    public void getTrelloBoards(@RequestParam(required = false) boolean filter) {
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        if(!filter){
+//            trelloBoards.forEach(trelloBoardDto -> {
+//                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//            });
+//        }else {
+//            trelloBoards.forEach(trelloBoardDto -> {
+//                if (!trelloBoardDto.getName().isEmpty() && trelloBoardDto.getName().contains("Kodilla") && !trelloBoardDto.getId().isEmpty()){
+//                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//                }
+//            });
+//        }
+//    }
+
+//    @GetMapping("boardsWithResponse")
+//    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoardsWithResponse(){
+//        return ResponseEntity.ok(trelloClient.getTrelloBoardsWithLogs());
+//    }
+
+//    @PostMapping("cardsWithResponse")
+//    public ResponseEntity<CreatedTrelloCard> createTrelloCardWithResponse(@RequestBody TrelloCardDto trelloCardDto){
+//        return ResponseEntity.ok(trelloClient.createNewCard(trelloCardDto));
+//    }
+
+
+//    @GetMapping("boards2")
+//    public List<TrelloBoardDto> getTrelloBoardsV2() {
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        trelloBoards.forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+//            System.out.println("This board contains lists: ");
+//            trelloBoardDto.getLists().forEach(trelloList -> {
+//                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+//            });
+//        });
+//        return trelloBoards;
+//    }
+//
+//    @PostMapping("cards")
+//    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+//        CreatedTrelloCard test=trelloClient.createNewCard(trelloCardDto);
+//        //for testing purpose
+//        System.out.println(test);
+//        return test;
+//
+//    }
 
 }
 
---
+++ src/main/java/com/crud/task/service/TrelloService.java
@@ -0,0 +1,39 @@
+package com.crud.task.service;
+
+import com.crud.task.config.AdminConfig;
+import com.crud.task.domain.CreatedTrelloCard;
+import com.crud.task.domain.Mail;
+import com.crud.task.domain.TrelloBoardDto;
+import com.crud.task.domain.TrelloCardDto;
+import com.crud.task.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+
+@Service
+@RequiredArgsConstructor
+public class TrelloService {
+    private static final String SUBJECT="Tasks: new Trello Card";
+    private final TrelloClient trelloClient;
+    private final SimpleEmailService emailService;
+    private final AdminConfig adminConfig;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+        ofNullable(newCard).ifPresent(card -> emailService.send(
+                new Mail(
+                        adminConfig.getAdminMail(),
+                        SUBJECT,
+                        "New card: " + trelloCardDto.getName() + " has been created on your Trello account",
+                        null
+                )));
+        return newCard;
+
+    }
+}
