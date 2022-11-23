94a89b1a73f362d0be4f2a66b81016e6a59a8f68
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-11-23 18:56:55 +0100

    33 thymeleaf mail changes

--- src/main/java/com/crud/task/service/MailCreatorService.java
+++ src/main/java/com/crud/task/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.task.service;
 
+import com.crud.task.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,8 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
-
+    @Autowired
+    private AdminConfig adminConfig;
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -16,6 +18,9 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://cokroktoupadek.github.io");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
